a46a332f9ed5240c7f0dc60f68b858dd7db8c7d9
arnold <114575651+arnulljs@users.noreply.github.com>
2024-11-20 12:59:58 +0800

    GUI Swing Implementation
    unfinished

---
+++ DiscussUsSignUp/src/discussussignup/DiscussUsSignUp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package discussussignup;
+
+/**
+ *
+ * @author 18020774cpe2101l-1
+ */
+public class DiscussUsSignUp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SignUpGUI register = new SignUpGUI();
+        register.setVisible(true);
+        register.pack();
+        register.setLocationRelativeTo(null);
+    }
+    
+}
